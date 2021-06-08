40c5ee9a363996c09e3307b97e374d179eee930d
ana bergese <violetagele22@gmail.com>
2021-06-08 17:07:06 +0200

    Controller for reservation added and new/create routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :celebrities
+  resources :reservations, only: [ :new, :create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/pages/components', to: 'pages#components', as: :components
 end
