488a07966346b1da43747bf69b21048e79bacd01
ana bergese <violetagele22@gmail.com>
2021-06-08 17:52:46 +0200

    Working in controller: done new, show and private method. Create method in progress.Added to reservations view: form, new, show. Added routes for reservations.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,25 @@
 class ReservationsController < ApplicationController
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user = current_user
+      if @reservation.save!
+        redirect_to celebrities_path
+      else
+        render :new
+      end
+  end
+
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:reserve_begin, :reserve_end, :status, :description)
+  end
 end
