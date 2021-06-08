f866df11c5b99bac558e860ea947d8143cb34a7d
Yanik Meili <yanikm41@gmail.com>
2021-06-08 15:39:16 +0200

    View for new and show: done. And, Controller for show:done

--- app/controllers/celebrities_controller.rb
+++ app/controllers/celebrities_controller.rb
@@ -3,4 +3,14 @@ class CelebritiesController < ApplicationController
   def index
   end
 
+  def show
+    @celebrity = Celebrity.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
 end
