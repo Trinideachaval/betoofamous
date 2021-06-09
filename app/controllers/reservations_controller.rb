a55b31dd1612c9b12bf76fb70b0106a22911f67c
ana bergese <violetagele22@gmail.com>
2021-06-09 10:48:07 +0200

    Controllers of reservations initiated, we need to finish all the methods and views

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,7 +6,7 @@ class ReservationsController < ApplicationController
   def create
     @reservation = Reservation.new(reservation_params)
     @reservation.user = current_user
-      if @reservation.save!
+a      if @reservation.save!
         redirect_to celebrities_path
       else
         render :new
