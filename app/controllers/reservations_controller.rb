243ca7209c5da3e22b73fbc0fb291d343d72e3e7
ana bergese <violetagele22@gmail.com>
2021-06-10 11:49:46 +0200

    The past to create and display the infor of reservation is done

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,13 +1,18 @@
 class ReservationsController < ApplicationController
+
   def new
     @reservation = Reservation.new
+    @celebrity = Celebrity.find(params[:celebrity_id])
   end
 
   def create
     @reservation = Reservation.new(reservation_params)
+    @celebrity = Celebrity.find(params[:celebrity_id])
+    @reservation.status = "pending"
     @reservation.user = current_user
+    @reservation.celebrity = @celebrity
       if @reservation.save!
-        redirect_to celebrities_path
+        redirect_to celebrity_reservation_path(@celebrity, @reservation)
       else
         render :new
       end
@@ -20,6 +25,6 @@ class ReservationsController < ApplicationController
   private
 
   def reservation_params
-    params.require(:reservation).permit(:reserve_begin, :reserve_end, :status, :description)
+    params.require(:reservation).permit(:reserve_begin, :reserve_end, :status)
   end
 end
