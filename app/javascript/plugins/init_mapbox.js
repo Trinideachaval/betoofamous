9dc6b3d1610fba45a1c68d50140fb5e0a65177ef
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-06-11 15:00:20 +0200

    Fix: fixed markers. removed temporarly

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,7 +5,6 @@ const buildMap = (mapElement) => {
   return new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v10',
-    center: [-74.5, 40], // starting position [lng, lat]
     zoom: 9 // starting zoom
   });
 };
